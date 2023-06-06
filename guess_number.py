4fc1e9e501a7e7ae50bd2954b7b7d6615548724d
Camila Aviles <camilanashv2003@gmail.com>
2023-06-06 13:19:55 -0600

    Feat: Se agrega el bucle while

--- guess_number.py
+++ guess_number.py
@@ -7,4 +7,21 @@ def guess_number():
 	numero_adivinar = randint(minimo, maximo)
 	print (numero_adivinar)
 	num = numero_adivinar - 1
+	while num!=numero_adivinar and puntos>0:
+		print(f"Adivina el numero entre: {minimo} y {maximo}: ")
+		num = int(input())
+		if num>numero_adivinar:
+			print("Muy alto")
+			puntos = puntos-10	
+			print("le quedad ",puntos," puntos")
+		elif num<numero_adivinar:
+			print("Muy bajo")
+			puntos = puntos-10	
+			print("le quedad ",puntos," puntos")
+
+	if puntos==0:
+		print("Game Over")
+	else:
+		print("exacto, les restaron", puntos,"puntos, eres un genio!")
+
 	
