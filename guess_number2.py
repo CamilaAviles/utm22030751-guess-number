bd3a9615559f2a15c5800f160f4e1d7a9e66ce8b
J. Pablo <jpablito022@gmail.com>
2023-06-07 21:51:40 -0600

    feat: player 2 must guess the number given by player 1, each time he makes a mistake, he will lose 10 points, and if his points run out, player 1 will be the winner.

--- guess_number2.py
+++ guess_number2.py
@@ -10,3 +10,19 @@ def guess_number_2():
         return
     points_player2 = 100 # player 2 has 100 inicial points
     points = 10
+    while points > 0:
+        print("\nYou have", points_player2, "points.")
+        maximum_num = int(input("Player 2, guess the number: ")) # Telling player 2 to guess the number decided by player 1
+        if maximum_num == secret_num: # If player's 2 number is the same as player's 1 number, print:
+            print("That's right player 2, you are a genius!")
+            break
+        else:
+            print("Incorrect number") # else print that it is not the number introduced by player 1
+            points_player2 -= 10 # substract 10 points each time player 2 doesn't guees the number
+            points -= 1
+    if points_player2 > 0:
+        print("You win with ", points_player2, "points") # Telling player 2 with how much points he/she win
+    else:
+        print("Player 1 wins!") # If not, player 1 is the winner 
+
+guess_number_2()
